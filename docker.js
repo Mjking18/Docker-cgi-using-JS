59e8c50d6c6a7db44f9ecdc76fe340d4c58b703d
Mjking18 <78278882+Mjking18@users.noreply.github.com>
2021-06-22 21:13:44 +0530

    Update docker.js

--- docker.js
+++ docker.js
@@ -2,7 +2,7 @@ function lw() {
   var i = document.getElementById("input").value;
   var xhr = new XMLHttpRequest();
 
-  xhr.open("GET", "http://192.168.43.229/cgi-bin/docker.py?cmd=" + i, true);
+  xhr.open("GET", "http://192.168.56.1/cgi-bin/docker.py?cmd=" + i, true);
   xhr.send();
   xhr.onload = function () {
     var output = xhr.responseText;
